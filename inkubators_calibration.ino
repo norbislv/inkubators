bb42403da943b2d9f82315afacb2f518eb5b4104
norbis <norbis@solarmachinery.lv>
2020-12-29 00:09:46 +0200

    function definitions

--- inkubators_calibration.ino
+++ inkubators_calibration.ino
@@ -1,12 +1,10 @@
 
-#include <dht.h>
-
-
+#include <DHT.h>
 
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 
-// DHT Sensor
-uint8_t DHTPin = D4; 
+int PWM=0; //current pwm
+uint8_t DHTPin = D4; // DHT Sensor
 const int heaterPin = 16; //D0
                
 // Initialize DHT sensor.
@@ -20,7 +18,7 @@ void setup() {
   delay(100);
   pinMode(DHTPin, INPUT);
   dht.begin();  
-//  analogWrite(heaterPin, 512);            
+//  analogWrite(heaterPin, 512);  // heater          
  }
 
  
@@ -31,12 +29,37 @@ void loop() {
   delay(3000);
 }
 
-int calibration(){ 
-// heats incubator in steps of 5% of pwm and checks when tempreature stops increasing. stores data on SPIFFS in calibration.txt file
-// outputs serial data: "temperature pwm" for plotting
+bool calibration(){ 
+// heats incubator in steps of 5% of pwm and checks when tempreature stops increasing. stores data on SPIFFS in calibration.txt file. checksum is added to file when calibration is complete.
+// outputs serial data in format: "temperature, pwm" for plotting or just watching it go brrrrrr. returns true when finished.
   
 }
 
-void init(){
+bool checkSPIFF(){
+//checks if version.txt is present, if not offers to format SPIFF. returns true if version.txt is present or format is complete.
   
+}
+
+bool checkCalibrtion(){
+//checks if calibration.txt if present, if not offers to calibrate. returns true if calibrated.
+}
+
+
+int checkWatchDog() {
+//checks if finished.txt contains time. if second line is "0" it means that cycle had finished. returns elpased time from the file or 0 if cycle had finished
+  
+}
+
+bool updateWatchDog (int elpased) {
+//stores elapsed time in finished.txt. returns ture on success
+}
+
+
+void startHeatCycle(int temperature, int timeToGo){
+// starts heat cycle
+// resets watchdog file.  
+}
+
+int checkTemperature (int targetTemperature, int previousTemperature){
+  //checks if current temprature is at target. looks at temperature difference between previous temperature and adjusts PWM.  
 }
