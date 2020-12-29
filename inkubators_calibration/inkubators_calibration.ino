8ef861ac65ac190b3ecb31f2a0a843cbfa5e107f
norbis <norbis@solarmachinery.lv>
2020-12-29 15:21:16 +0200

    WIP

---
+++ inkubators_calibration/inkubators_calibration.ino
@@ -0,0 +1,66 @@
+
+#include <DHT.h>
+
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+
+//global variables
+int PWM=0; //current pwm
+uint8_t DHTPin = D4; // DHT Sensor
+const int heaterPin = 16; //D0
+int operation=0; //what operation controller is currently doing
+               
+// Initialize DHT sensor.
+DHT dht(DHTPin, DHTTYPE);                
+ 
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+  pinMode(DHTPin, INPUT);
+  dht.begin();        
+ }
+ 
+void loop() {
+  delay(3000);
+  checkTemperature(30,30);
+}
+
+bool calibration(){ 
+// heats incubator in steps of 5% of pwm and checks when tempreature stops increasing. stores data on SPIFFS in calibration.txt file. checksum is added to file when calibration is complete.
+// outputs serial data in format: "temperature, pwm" for plotting or just watching it go brrrrrr. returns true when finished.
+
+}
+
+bool checkSPIFF(){
+//checks if version.txt is present, if not offers to format SPIFF. returns true if version.txt is present or format is complete.
+  
+}
+
+bool checkCalibrtion(){
+//checks if calibration.txt if present, if not offers to calibrate. returns true if calibrated.
+}
+
+
+int checkWatchDog() {
+//checks if finished.txt contains time. if second line is "0" it means that cycle had finished. returns elpased time from the file or 0 if cycle had finished
+  
+}
+
+bool updateWatchDog (int elpased) {
+//stores elapsed time in finished.txt. returns ture on success
+}
+
+
+void startHeatCycle(int temperature, int timeToGo){
+// starts heat cycle
+// resets watchdog file.  
+}
+
+int checkTemperature (int targetTemperature, int previousTemperature){
+//checks if current temprature is at target. looks at temperature difference between previous temperature and adjusts PWM. returns current temperature
+ float temperature;
+ float humidity;
+ temperature = dht.readTemperature(); // Gets the values of the temperature
+ humidity = dht.readHumidity(); // Gets the values of the humidity 
+ Serial.println(temperature); 
+ //  analogWrite(heaterPin, 512);  // heater 
+}
