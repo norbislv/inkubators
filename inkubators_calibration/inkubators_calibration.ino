3fa0252217672fb421fcb5b4bc222078f4970b25
norbis <norbis@solarmachinery.lv>
2020-12-30 12:14:22 +0200

    calibration routine

--- inkubators_calibration/inkubators_calibration.ino
+++ inkubators_calibration/inkubators_calibration.ino
@@ -7,7 +7,7 @@
 const uint8_t vers= 0; //version number
 uint8_t DHTPin = D4; // DHT Sensor
 const uint8_t heaterPin = 16; //D0
-uint8_t operation=0; //what operation controller is currently doing
+uint8_t operation=0; //what operation controller is currently doing. 0 - idle, 1 - heating cycle, 2 - calibration
 int cycleStartTime=0; //time when cycle was started
 int PWM=0; //output pwm value 
                
@@ -94,7 +94,7 @@ bool formatSPIFFS(){
     Serial.println("file open failed");
     return false;
   }
-  f.print("0");
+  f.println("0");
   Serial.println("done");
   f.close();
   return true;
@@ -107,7 +107,8 @@ bool doSetup(){
     Serial.println("file open failed");
     return false;
   }
-  f.print("40"); //maximum temperature. ask user for this in future
+  f.println("40"); //maximum temperature. ask user for this in future
+  f.println("256"); //starting pwm. one quarter of maximum
   Serial.println("done");
   f.close();
   return true;
@@ -116,7 +117,66 @@ bool doSetup(){
 bool calibration(){ 
 // heats incubator in steps of 5% of pwm and checks when tempreature stops increasing. stores data on SPIFFSS in calibration.txt file. checksum is added to file when calibration is complete.
 // outputs serial data in format: "temperature, pwm" for plotting or just watching it go brrrrrr. returns true when finished.
+  File f = SPIFFS.open("/setup.txt", "r");
+  if (!f) {
+    Serial.println("file open failed");
+    return false;
+  }
+   operation = 2;
+   int pwmStep=102;//will increase in steps of ~10%
+   int temperature = 0; 
+   int steadyCounter=0;
+   
+   String s=f.readStringUntil('\n'); 
+   int maxTemp = s.toInt();
+   s=f.readStringUntil('\n');
+   PWM = s.toInt();//starting PWM
+   f.close();
+   f = SPIFFS.open("/calibration.txt", "w");
+   if (!f) {
+     Serial.println("file open failed");
+     return false;
+   }
+   
+  
+   int oldTemp = dht.readTemperature(); //we start from current temperature, not 0
+   analogWrite(heaterPin, PWM);  // start the heater 
+   
+   while (temperature<maxTemp){//run until we reaxh max temp. 
+     delay(3000); //sensor needs about 2 seconds between reads
+     temperature = dht.readTemperature();
+     Serial.print (temperature); 
+     Serial.print (", ");
+     Serial.print (PWM);
+     if ((temperature-oldTemp)<1) steadyCounter++;
+     if (steadyCounter>20){//temperature hasn't changed much for one minute
+      if (PWM==2023) { //calibration complete
+        f.print(PWM);
+        f.print(" ");
+        f.print(temperature);
+        PWM=0;
+        analogWrite(heaterPin, PWM);
+        break;
+        
+      }
+      if (PWM<(1023-pwmStep)){
+        f.print(PWM);
+        f.print(" ");
+        f.print(temperature);
+        PWM+=pwmStep;
+        analogWrite(heaterPin, PWM);
 
+      }
+      else{
+        f.print(PWM);
+        f.print(" ");
+        f.print(temperature);
+        PWM=2023;
+        analogWrite(heaterPin, PWM);
+        
+      }
+     }
+   }
 }
 
 
@@ -142,12 +202,12 @@ void initController(){
   if (!checkCalibration()){
     Serial.println("Calibration should be done. This will take long time. proceed? [Y/n]");
     int incomingByte= 0;
-    while (Serial.available() == 0) {
+    while (Serial.available() == 0) {//wait for user input
   }
-  incomingByte = Serial.read();
-  if (char(incomingByte)=='Y' || char(incomingByte)=='y') {
-  calibration();
-  }
+    incomingByte = Serial.read();
+    if (char(incomingByte)=='Y' || char(incomingByte)=='y') {
+    calibration();
+    }
   else {
     Serial.println("ok, will just read sensors in main loop for now");
    }
